ac72d63b4f9b3339aecdad055f1f45a5d7737aa2
Nathan Cowley-Cooper <nathancowleycooper@gmail.com>
2020-11-10 20:05:50 +0800

    updated Nav

--- src/components/Navigation/nav.jsx
+++ src/components/Navigation/nav.jsx
@@ -7,15 +7,15 @@ function Nav() {
   return (
     <nav>
       <div id="navigtaion">
-        <NavLink activeClassName="navbar__link--active" to= "/home" id="navIcons" style={{textDecoration: 'none'}}>
+        <NavLink activeClassName="mainNav__link--active" to= "/home" id="navIcons" style={{textDecoration: 'none'}}>
             <img src="/images/home.svg" className="svgicon" alt="Home Icon"/>
             <h5>Home</h5>
         </NavLink>
-        <NavLink activeClassName="navbar__link--active" to= "/progress" id="navIcons" style={{textDecoration: 'none'}}>
+        <NavLink activeClassName="mainNav__link--active" to= "/progress" id="navIcons" style={{textDecoration: 'none'}}>
             <img src="/images/progress.svg" className="svgicon" alt="Progress Icon"/>
             <h5>Progress</h5>
         </NavLink>
-        <NavLink activeClassName="navbar__link--active" to= "/menu" id="navIcons" style={{textDecoration: 'none'}}>
+        <NavLink activeClassName="mainNav__link--active" to= "/menu" id="navIcons" style={{textDecoration: 'none'}}>
             <img src="/images/menu.svg" className="svgicon" alt="Menu Icon" />
             <h5>Menu</h5>
         </NavLink>
